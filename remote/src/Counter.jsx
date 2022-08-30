8d1240f7fbb3ece67afdb32574cb3afe36b7a6d2
harihar99 <apamarjane60@gmail.com>
2022-08-30 13:08:22 +0530

    Added tailwind css

--- remote/src/Counter.jsx
+++ remote/src/Counter.jsx
@@ -4,9 +4,11 @@ export default()=>{
     const [count, setCount] =createSignal(0);
 
     return(
-        <div>
-            <div>Count ={count()}</div>
-            <button onClick={()=>setCount(count()+1)}>Add one</button>
+        <div class="bg-blue-900 text-white p-6 font-serif ">
+            <div>
+                <h1 class="text-center">Count = {count()}</h1>
+            </div>
+            <button  class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={()=>setCount(count()+1)}>Add one</button>
         </div>
     )
 }
